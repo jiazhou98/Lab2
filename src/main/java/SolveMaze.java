38db5d11d45999ef8386f2ea7b75e74c12dbf5ba
Jia Zhou <jiaz2@illinois.edu>
2018-01-31 12:41:52 -0600

    done

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,14 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                maze.turnLeft();
+                while (maze.canMove() == false) {
+                    maze.turnRight();
+                }
+                maze.move();
+                System.out.println(maze);
+            }
         }
 
         if (maze.isFinished()) {
